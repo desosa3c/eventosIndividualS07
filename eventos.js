b744c53a38b0970db7b2ca75f12a59624f1e8c7e
desosa3c <desosaf3c@gmail.com>
2023-08-05 20:57:28 -0300

    agregue un alert en el html y elimine un evento del script

--- eventos.js
+++ eventos.js
@@ -1,9 +1,9 @@
 const btnSaludo = document.querySelector('.btnSaludo');
 const divSaludo = document.querySelector('.saludoDiv');
 
-btnSaludo.onclick = function () {
+/*btnSaludo.onclick = function () {
     alert('Hola!');
-};
+};*/
 
 divSaludo.addEventListener('click', function () {
     alert('Hola! Soy el div');
